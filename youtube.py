72a23ac20fc948ebeb103ea669279128c58c188c
Yonatan <yonatanfilar@gmail.com>
2024-03-23 22:48:01 +0200

    Add function that finds bad comments & refactor & add video_id column

--- youtube.py
+++ youtube.py
@@ -7,9 +7,9 @@ class Comments:
         self.bad_words = set(['kike', 'shylock', 'coloniser', 'colonizer', 'from the river to the sea', 'zionazi', 'hitler', 'holocaust', 'rothschild', 'lehi', 'ethnic cleansing'])
         self.video_id = video_id
 
-    def persist_to_database(self, comment, author_name, comment_id, potentially_antisemitic = False):
-        query = f"""INSERT INTO comments(text, author, comment_id, potentially_antisemitic)
-                VALUES ('{comment}', '{author_name}', '{comment_id}', {potentially_antisemitic})"""
+    def persist_to_database(self, comment, author_name, video_id, comment_id, potentially_antisemitic = False):
+        query = f"""INSERT INTO comments(text, author, video_id, comment_id, potentially_antisemitic)
+                VALUES ('{comment}', '{author_name}', '{video_id}', '{comment_id}', {potentially_antisemitic})"""
         
         self.run_query(query)
 
@@ -28,9 +28,9 @@ class Comments:
                 replycount = item['snippet']['totalReplyCount']
 
                 if any(word in comment for word in self.bad_words):
-                    self.persist_to_database(comment, author_name, comment_id, True)
+                    self.persist_to_database(comment, author_name, self.video_id, comment_id, True)
                 else:
-                    self.persist_to_database(comment, author_name, comment_id)
+                    self.persist_to_database(comment, author_name, self.video_id, comment_id)
         
             if replycount > 0 and 'replies' in item:
                     replies = item['replies']['comments']
@@ -39,9 +39,9 @@ class Comments:
                         reply_author_name = reply['snippet']['authorDisplayName']
                         reply_id = reply['id']
                         if any(word in reply_text for word in self.bad_words):
-                            self.persist_to_database(reply_text, reply_author_name, reply_id, True)
+                            self.persist_to_database(reply_text, reply_author_name, self.video_id, reply_id, True)
                         else:
-                            self.persist_to_database(comment, author_name, comment_id)
+                            self.persist_to_database(comment, author_name, self.video_id, comment_id)
                         
             if 'nextPageToken' in video_response:
                 video_response = youtube.commentThreads().list(
@@ -52,14 +52,26 @@ class Comments:
             else:
                 break
 
-    def run_query(self, query):
+    def find_bad_comments(self):
+        query = "SELECT * FROM comments WHERE potentially_antisemitic = True"
+       
+        comments = self.run_query(query, fetch_results=True)
+        return comments
+
+    def run_query(self, query, fetch_results=False):
         conn = psycopg2.connect(host=config.HOSTNAME, user=config.USERNAME, dbname=config.DATABASE)
         cursor = conn.cursor()
         cursor.execute(query)
+        if fetch_results:
+            result = cursor.fetchall()
+        else:
+            result = None
         conn.commit()
         conn.close()
         cursor.close
-          
+        return result
 
-comments = Comments('lJYn09tuPw4')
-comments.video_comments()
+    
+# comments = Comments('lJYn09tuPw4')
+# comments.video_comments()
+# comments.find_bad_comments()
